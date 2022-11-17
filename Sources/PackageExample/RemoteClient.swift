5f2ea56945d61e87be317c0283cd8eda6e352f31
Sam Rowley <srowley3@jaguarlandrover.com>
2022-11-17 09:42:15 +0000

    Implement RemoteClient with fake methods to conform to the protocol

--- Sources/PackageExample/RemoteClient.swift
+++ Sources/PackageExample/RemoteClient.swift
@@ -1,7 +1,28 @@
 import Foundation
 import AdapterExample
+import Combine
 
 /// Predefined topics to publish and subscribe to
 struct RemoteClient: Client {
+    var connectionState: AnyPublisher<Bool, Never>
     
+    func connect() -> AnyPublisher<Void, AdapterExample.AdapterError> {
+        return Fail(error: AdapterError.testError).eraseToAnyPublisher()
+    }
+    
+    func disconnect() -> AnyPublisher<Void, Never> {
+        return Just(Void()).eraseToAnyPublisher()
+    }
+    
+    func subscribe(to topic: String) -> AnyPublisher<Data, AdapterExample.AdapterError> {
+        return Fail(error: AdapterError.testError).eraseToAnyPublisher()
+    }
+    
+    func publish(_ value: Data, to topic: String) -> AnyPublisher<Void, AdapterExample.AdapterError> {
+        return Fail(error: AdapterError.testError).eraseToAnyPublisher()
+    }
+    
+    func publish(_ value: Data, to topic: String) async throws {
+        
+    }
 }
